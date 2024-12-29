b5d2fb97c201ebd352796438ac647b0308f273aa
Large-Mac <bighaodwofad@gmail.com>
2024-12-28 22:45:35 -0800

    patchless DWM build with pywal and using default bar

---
+++ setbglight.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Choosing a random wallpaper ..."
+wall=$(find $HOME/Pictures/Wallpapers -type f | shuf -n 1)
+echo "Setting wallpaper ..."
+xwallpaper --zoom $wall
+echo "Generating pywal color-scheme ..."
+wal -l -i $wall >/dev/null
+sed -i 'N;$!P;D' $HOME/.cache/wal/colors-wal-dwm.h
+echo "Recompiling DWM with colors ..."
+(cd $HOME/.config/suckless/dwm && sudo make clean install >/dev/null)
+echo "Restart DWM for new colors! (Mod-Shft-Ctrl-q)"
