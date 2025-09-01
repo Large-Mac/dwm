0fbca3e1d1af16f0e64de9198c37ce15e6033f88
glenda <glenda@glenda>
2025-09-01 01:25:22 -0700

    added swallow gui by default

---
+++ patch/include.c
@@ -0,0 +1,30 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+#include "bar.c"
+
+#include "bar_ltsymbol.c"
+#include "bar_status.c"
+#include "bar_statuscmd.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+
+/* Other patches */
+#include "recttoclient.c"
+#include "banish.c"
+#include "cyclelayouts.c"
+#include "fakefullscreenclient.c"
+#include "keymodes.c"
+#include "shift.c"
+#include "shiftview.c"
+#include "stacker.c"
+#include "swallow.c"
+#include "vanitygaps.c"
+#include "xresources.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_centeredfloatingmaster.c"
+#include "layout_flextile-deluxe.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
---
+++ patch/include.h
@@ -0,0 +1,29 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_statuscmd.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+
+/* Other patches */
+#include "recttoclient.h"
+#include "banish.h"
+#include "cyclelayouts.h"
+#include "fakefullscreenclient.h"
+#include "keymodes.h"
+#include "shift.h"
+#include "shiftview.h"
+#include "stacker.h"
+#include "swallow.h"
+#include "vanitygaps.h"
+#include "xresources.h"
+/* Layouts */
+#include "layout_centeredfloatingmaster.h"
+#include "layout_flextile-deluxe.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
